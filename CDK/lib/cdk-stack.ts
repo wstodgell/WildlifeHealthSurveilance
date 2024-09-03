42bcbc1c99f9f963183bd58fc58ea13d20e1f546
wstodgell <curtisstodgell2@gmail.com>
2024-09-03 15:54:27 -0700

    addded stuff for github actions

--- CDK/lib/cdk-stack.ts
+++ CDK/lib/cdk-stack.ts
@@ -2,19 +2,22 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
 
-export class MyIotCdkProjectStack extends cdk.Stack {
+export class CdkStack extends cdk.Stack {
+  public readonly ecrRepositoryUri: string;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Create an ECR repository
-    const myEcrRepo = new ecr.Repository(this, 'MyIotGpsAppRepository', {
-      repositoryName: 'my-iot-gps-app',
+    const repository = new ecr.Repository(this, 'MyIotGpsAppRepository', {
+      repositoryName: 'my-iot-gps-app'
     });
 
-    // Output the URI of the ECR repository
-    new cdk.CfnOutput(this, 'MyIotGpsAppRepositoryUri', {
-      value: myEcrRepo.repositoryUri,
+    this.ecrRepositoryUri = repository.repositoryUri;
+
+    new cdk.CfnOutput(this, 'EcrRepositoryUri', {
+      value: this.ecrRepositoryUri,
       description: 'URI of the ECR repository',
+      exportName: 'EcrRepositoryUri'
     });
   }
 }
