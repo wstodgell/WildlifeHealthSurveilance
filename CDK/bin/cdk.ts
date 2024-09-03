630b7cc897561441ab87d1a37e7d231a8c016a91
wstodgell <curtisstodgell2@gmail.com>
2024-09-03 16:01:36 -0700

    modified stack names

--- CDK/bin/cdk.ts
+++ CDK/bin/cdk.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { MyIotCdkProjectStack } from '../lib/cdk-stack';
+import { CdkStack } from '../lib/cdk-stack';
 
 const app = new cdk.App();
-new MyIotCdkProjectStack(app, 'CdkStack', {
+new CdkStack(app, 'IoTStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
